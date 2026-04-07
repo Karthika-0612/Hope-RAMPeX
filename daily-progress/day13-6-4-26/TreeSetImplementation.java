ddd3afb28ba23a88418dbc41ada14ce2084b9302
Karthika-0612 <karthikavenkatachalam06@gmail.com>
2026-04-07 21:28:02 +0530

    new files

--- daily-progress/day13-6-4-26/TreeSetImplementation.java
+++ daily-progress/day13-6-4-26/TreeSetImplementation.java
@@ -1,12 +1,14 @@
 import java.util.*;
 public class TreeSetImplementation {
     public static void main(String[] args) {
-        Set<Integer> set=new TreeSet<>();
+        TreeSet<Integer> set=new TreeSet<>();
         set.add(30);
         set.add(20);
         set.add(10);
         set.add(15);
         set.add(10);
+        set.tailSet(5);
+        set.headSet(5);
         System.out.println(set);
         
     }
