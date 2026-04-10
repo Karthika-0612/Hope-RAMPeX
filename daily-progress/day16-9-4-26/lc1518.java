01c7d7f6975cf09088441d375f79a31aaebe940e
Karthika-0612 <karthikavenkatachalam06@gmail.com>
2026-04-10 10:24:13 +0530

    daily progress

---
+++ daily-progress/day16-9-4-26/lc1518.java
@@ -0,0 +1,18 @@
+//LeetCode 1518. Water Bottles
+public class lc1518 {
+    public static int numWaterBottles(int numBottles, int numExchange) {
+        int total=numBottles;
+        while(numBottles>=numExchange){
+            int empty=numBottles/numExchange;
+            total+=empty;
+            numBottles=empty+numBottles%numExchange;
+        }
+        return total;
+    }  
+    public static void main(String[] args) {
+        int numBottles=9;
+        int numExchange=3;
+        System.out.println(numWaterBottles(numBottles, numExchange));
+    } 
+
+}
---
+++ daily-progress/day16-9-4-26/lc2600.java
@@ -0,0 +1,24 @@
+//LeetCode 2600. K Items With the Maximum Sum
+public class lc2600 {
+    public static int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int sum=0;
+        if(k<=numOnes){
+            sum=k;
+        }
+        else if(k>numOnes && k<=numOnes+numZeros){
+            sum=numOnes;
+        }
+        else{
+            sum=numOnes-(k-numOnes-numZeros);
+        }
+        return sum;
+    }  
+    public static void main(String[] args) {
+        int numOnes=3;
+        int numZeros=2;
+        int numNegOnes=0;
+        int k=2;
+        System.out.println(kItemsWithMaximumSum(numOnes, numZeros, numNegOnes, k));
+    }
+    
+}
